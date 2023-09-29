c634795a628ec736d725f442dac3fb41b7578abd
rhaynacasado <128262538+rhaynacasado@users.noreply.github.com>
2023-09-29 06:02:05 -0300

    Aprimoramento das funcionalidades 1 e 2
    Contagem de tecnologias e pares de tecnologias concluido com sucesso (optouse por fazer a contagem em memória de disco), atualização do cabeçalho e minor changes

--- T_zero.c
+++ T_zero.c
@@ -9,7 +9,7 @@
 #define TAM_REGISTRO_VARIAVEL 55
 #define TAM_CABECALHO 13
 
-#define NULO '-1'
+#define NULO "-1"
 #define LIXO "$"
 
 #define REMOVIDO '1'
@@ -39,17 +39,25 @@ typedef struct {
 } Registro;
 
 void escreveCabecalho(FILE* arquivo, Cabecalho cabecalho){
-  // fseek(arquivo, 1, SEEK_SET);
+  fseek(arquivo, 0, SEEK_SET);
   fwrite(&cabecalho.status, sizeof(char), 1, arquivo);
   fwrite(&cabecalho.proxRRN, sizeof(int), 1, arquivo);
   fwrite(&cabecalho.nroTecnologias, sizeof(int), 1, arquivo);
   fwrite(&cabecalho.nroParesTecnologias, sizeof(int), 1, arquivo);
-
 }
 
-//implementar a contagem de tecnologias e paresdetecnologias
+void printCabecalho(FILE* arquivo){
+  fseek(arquivo, 0, SEEK_SET);
+  Cabecalho cabecalho;
+  fread(&cabecalho.status, sizeof(char), 1, arquivo);
+  fread(&cabecalho.proxRRN, sizeof(int), 1, arquivo);
+  fread(&cabecalho.nroTecnologias, sizeof(int), 1, arquivo);
+  fread(&cabecalho.nroParesTecnologias, sizeof(int), 1, arquivo);
+  printf("%c %d %d %d\n", cabecalho.status, cabecalho.proxRRN, cabecalho.nroTecnologias, cabecalho.nroParesTecnologias);
+}
 
-int contaTecnologias(char* novaTecOrigem, char* novaTecDestino){ 
+
+int contaTecnologias(FILE* arquivoBinario, char* novaTecOrigem, char* novaTecDestino){ 
   /*
   retornos:
   
@@ -60,13 +68,16 @@ int contaTecnologias(char* novaTecOrigem, char* novaTecDestino){
   4: origem, destino e par novos;
   */
 
-  FILE* arquivoBinario = fopen("arquivoBinario.bin", "rb");
   Registro registro;
   int tecRepetidaOrigem = 0;
   int tecRepetidaDestino = 0;
   int parRepetido = 0;
 
-  fseek(arquivoBinario, TAM_CABECALHO, SEEK_SET);
+  if (fseek(arquivoBinario, TAM_CABECALHO, SEEK_SET) != 0) {
+        printf("Falha no processamento do arquivo.\n");
+        fclose(arquivoBinario);
+        return -1;
+    }
   
   while(1){
     if(fread(&registro.removido, sizeof(char), 1, arquivoBinario) == 0){
@@ -78,12 +89,6 @@ int contaTecnologias(char* novaTecOrigem, char* novaTecDestino){
     }
 
     fseek(arquivoBinario, 12, SEEK_CUR);
-    // fread(&registro.grupo, sizeof(int), 1, arquivoBinario);
-    
-    // fread(&registro.popularidade, sizeof(int), 1, arquivoBinario);
-    
-    // fread(&registro.peso, sizeof(int), 1, arquivoBinario);
-
     
     fread(&registro.TecnologiaOrigem.tamanho, sizeof(int), 1, arquivoBinario);
 
@@ -100,58 +105,26 @@ int contaTecnologias(char* novaTecOrigem, char* novaTecDestino){
     int TAM_BITES = sizeof(registro.grupo)+sizeof(registro.popularidade)+sizeof(registro.peso)+sizeof(registro.TecnologiaOrigem.tamanho)+sizeof(registro.TecnologiaDestino.tamanho)+sizeof(char)+registro.TecnologiaOrigem.tamanho+registro.TecnologiaDestino.tamanho;
     fseek(arquivoBinario, TAM_REGISTRO - TAM_BITES + 1, SEEK_CUR);
 
-    // printf("novaorigem: %s, novodestino: %s, origem: %s, destino: %s\n", novaTecOrigem, novaTecDestino, registro.TecnologiaOrigem.string, registro.TecnologiaDestino.string);
-
-    printf("strcmp: %s e %s\n", registro.TecnologiaOrigem.string, novaTecOrigem);
     if(strcmp(registro.TecnologiaOrigem.string, novaTecOrigem) == 0){
       if(strcmp(registro.TecnologiaDestino.string, novaTecDestino) == 0){
-        fclose(arquivoBinario);
         return 0;
       }
       tecRepetidaOrigem = 1;
-      if(strcmp(registro.TecnologiaOrigem.string, novaTecDestino) == 0)
-        tecRepetidaDestino = 1;
     }
-    else if(tecRepetidaOrigem == 0 && strcmp(registro.TecnologiaDestino.string, novaTecOrigem) == 0)
+    if(tecRepetidaDestino == 0 && strcmp(registro.TecnologiaDestino.string, novaTecDestino) == 0){
+      tecRepetidaDestino = 1;
+    }
+    if(tecRepetidaDestino == 0 && strcmp(registro.TecnologiaOrigem.string, novaTecDestino) == 0){
+      tecRepetidaDestino = 1;
+    }
+    if(tecRepetidaOrigem == 0 && strcmp(registro.TecnologiaDestino.string, novaTecOrigem) == 0){
       tecRepetidaOrigem = 1;
+    }
     
     free(registro.TecnologiaOrigem.string);
     free(registro.TecnologiaDestino.string);
   }
 
-  // while(!feof(arquivoBinario)){
-  //   fread(&registro.removido, sizeof(char), 1, arquivoBinario);
-  //   if (registro.removido == '0'){
-  //     fseek(arquivoBinario, 12, SEEK_CUR);
-  //     fread(&registro.TecnologiaOrigem.tamanho, sizeof(int), 1, arquivoBinario);
-  //     fread(&registro.TecnologiaOrigem.tamanho, sizeof(int), 1, arquivoBinario);
-  //     registro.TecnologiaOrigem.string = malloc((registro.TecnologiaOrigem.tamanho + 1) * sizeof(char));
-  //     fread(registro.TecnologiaOrigem.string, sizeof(char), registro.TecnologiaOrigem.tamanho, arquivoBinario);
-  //     registro.TecnologiaOrigem.string[registro.TecnologiaOrigem.tamanho] = '\0';
-  //     fread(&registro.TecnologiaDestino.tamanho, sizeof(int), 1, arquivoBinario);
-  //     fread(&registro.TecnologiaDestino.tamanho, sizeof(int), 1, arquivoBinario);
-  //     registro.TecnologiaDestino.string = malloc((registro.TecnologiaDestino.tamanho + 1) * sizeof(char));
-  //     fread(registro.TecnologiaDestino.string, sizeof(char), registro.TecnologiaDestino.tamanho, arquivoBinario);
-  //     registro.TecnologiaDestino.string[registro.TecnologiaDestino.tamanho] = '\0';
-
-  //     // printf("novaorigem: %s, novodestino: %s, origem: %s, destino: %s\n", novaTecOrigem, novaTecDestino, registro.TecnologiaOrigem.string, registro.TecnologiaDestino.string);
-
-  //     printf("strc %s e %s\n", registro.TecnologiaOrigem.string, novaTecOrigem);
-  //     if(strcmp(registro.TecnologiaOrigem.string, novaTecOrigem) == 0){
-  //       if(strcmp(registro.TecnologiaDestino.string, novaTecDestino) == 0)
-  //         return 0;
-  //       tecRepetidaOrigem = 1;
-  //       if(strcmp(registro.TecnologiaOrigem.string, novaTecDestino) == 0)
-  //         tecRepetidaDestino = 1;
-  //     }
-  //     else if(tecRepetidaOrigem == 0 && strcmp(registro.TecnologiaDestino.string, novaTecOrigem) == 0)
-  //       tecRepetidaOrigem = 1;
-    
-  //     free(registro.TecnologiaOrigem.string);
-  //     free(registro.TecnologiaDestino.string);
-  //   }
-  // }
-  fclose(arquivoBinario);
   if(tecRepetidaOrigem == 1){
     if(tecRepetidaDestino == 1){
       return 1;
@@ -168,7 +141,7 @@ int contaTecnologias(char* novaTecOrigem, char* novaTecDestino){
 void createTable(FILE *arquivoEntrada, char** colunas) {
   //ler casos nulos
   
-  FILE *arquivoBinario = fopen("arquivoBinario.bin", "wb");
+  FILE *arquivoBinario = fopen("arquivoBinario.bin", "wb+");
 
   if (arquivoBinario == NULL) {
     printf("Falha no processamento do arquivo.\n");
@@ -189,107 +162,117 @@ void createTable(FILE *arquivoEntrada, char** colunas) {
     exit(-3);
   }
   
-  Registro registrosAtual;
+  Registro registroAtual;
   
   fgets(linha, TAM_REGISTRO, arquivoEntrada); //le e ignora a primeira linha do csv
   char* campo = strtok(linha, ",");
 
   for (int i = 0; i < 5; i++){
+    if(i == 4)
+      campo[strlen(campo) - 2] = '\0'; 
     colunas[i] = strdup(campo);
     campo = strtok(NULL, ",");
   }  
 
   while(fgets(linha, TAM_REGISTRO, arquivoEntrada) != NULL){
-    registrosAtual.removido = '0';
+    registroAtual.removido = '0';
 
     char* campo = strtok(linha, ",");
-
+    char c;
+    
     for(int i = 0; i < 5; i++){
+      // printf("campo %s\n", campo);
       if(strcmp(colunas[i], "nomeTecnologiaOrigem") == 0){
-          registrosAtual.TecnologiaOrigem.tamanho = strlen(campo);
-          registrosAtual.TecnologiaOrigem.string = campo;
+          registroAtual.TecnologiaOrigem.tamanho = strlen(campo);
+          registroAtual.TecnologiaOrigem.string = campo;
       }
       else if(strcmp(colunas[i], "nomeTecnologiaDestino") == 0){
-          registrosAtual.TecnologiaDestino.tamanho = strlen(campo);
-          registrosAtual.TecnologiaDestino.string = campo;
+          registroAtual.TecnologiaDestino.tamanho = strlen(campo);
+          registroAtual.TecnologiaDestino.string = campo;
       }
       else if(strcmp(colunas[i], "grupo") == 0)
-        registrosAtual.grupo = atoi(campo);
+        registroAtual.grupo = atoi(campo);
       else if(strcmp(colunas[i], "popularidade") == 0)
-        registrosAtual.popularidade = atoi(campo);
-      else if(strcmp(colunas[i], "peso") == 0)
-        registrosAtual.peso = atoi(campo);
+        registroAtual.popularidade = atoi(campo);
+      else if(strcmp(colunas[i], "peso") == 0){
+        registroAtual.peso = atoi(campo);
+      }
 
       campo = strtok(NULL, ",");
+      // printf("%s\n", campo);
     }
 
-    // if(registrosAtual.TecnologiaOrigem.tamanho > 0){
-    //   if(registrosAtual.TecnologiaDestino.tamanho > 0)
-        // contaTecnologias(arquivoBinario, registrosAtual.TecnologiaOrigem.string, NULL);
+    // if(registroAtual.TecnologiaOrigem.tamanho > 0){
+    //   if(registroAtual.TecnologiaDestino.tamanho > 0)
+        // contaTecnologias(arquivoBinario, registroAtual.TecnologiaOrigem.string, NULL);
     //   else
-        // int novaTec = contaTecnologias(registrosAtual.TecnologiaOrigem.string, registrosAtual.TecnologiaDestino.string);
+        int novaTec = contaTecnologias(arquivoBinario, registroAtual.TecnologiaOrigem.string, registroAtual.TecnologiaDestino.string);
     // }
-    // else if(registrosAtual.TecnologiaDestino.tamanho > 0)
-    //   contaTecnologias(arquivoBinario, NULL, registrosAtual.TecnologiaDestino.string);
+    // else if(registroAtual.TecnologiaDestino.tamanho > 0)
+    //   contaTecnologias(arquivoBinario, NULL, registroAtual.TecnologiaDestino.string);
 
-      // printf("novatec: %d, origem: %s, destino: %s\n", novaTec, registrosAtual.TecnologiaOrigem.string, registrosAtual.TecnologiaDestino.string);
-    // switch(novaTec){
-    //   case 0:
-    //     break;
-    //   case 1:
-    //     cabecalho.nroParesTecnologias++;
-    //     break;
-    //   case 2:
-    //     cabecalho.nroParesTecnologias++;
-    //     cabecalho.nroTecnologias++;
-    //     break;
-    //   case 3:
-    //     cabecalho.nroParesTecnologias++;
-    //     cabecalho.nroTecnologias++;
-    //     break;
-    //   case 4:
-    //     cabecalho.nroParesTecnologias++;
-    //     cabecalho.nroTecnologias += 2;
-    //     break;
-    // }
+    // printf("novatec: %d, %s\n", novaTec, registroAtual.TecnologiaOrigem.string);
+    switch(novaTec){
+      case 0:
+        break;
+      case 1:
+        cabecalho.nroParesTecnologias++;
+        break;
+      case 2:
+        cabecalho.nroParesTecnologias++;
+        cabecalho.nroTecnologias++;
+        break;
+      case 3:
+        cabecalho.nroParesTecnologias++;
+        cabecalho.nroTecnologias++;
+        break;
+      case 4:
+        cabecalho.nroParesTecnologias++;
+        cabecalho.nroTecnologias += 2;
+        break;
+    }
     
-    fwrite(&registrosAtual.removido, sizeof(char), 1, arquivoBinario);
-    fwrite(&registrosAtual.grupo, sizeof(int), 1, arquivoBinario);
-    fwrite(&registrosAtual.popularidade, sizeof(int), 1, arquivoBinario);
-    fwrite(&registrosAtual.peso, sizeof(int), 1, arquivoBinario);
-    fwrite(&registrosAtual.TecnologiaOrigem.tamanho, sizeof(int), 1, arquivoBinario);
-    fwrite(registrosAtual.TecnologiaOrigem.string, sizeof(char), registrosAtual.TecnologiaOrigem.tamanho, arquivoBinario);
-    fwrite(&registrosAtual.TecnologiaDestino.tamanho, sizeof(int), 1, arquivoBinario);
-    fwrite(registrosAtual.TecnologiaDestino.string, sizeof(char), registrosAtual.TecnologiaDestino.tamanho, arquivoBinario);
-    int tam_bytes = sizeof(registrosAtual.grupo)+sizeof(registrosAtual.popularidade)+sizeof(registrosAtual.peso)+sizeof(registrosAtual.TecnologiaOrigem.tamanho)+sizeof(registrosAtual.TecnologiaDestino.tamanho)+sizeof(char)+strlen(registrosAtual.TecnologiaDestino.string)+strlen(registrosAtual.TecnologiaOrigem.string);
+    fwrite(&registroAtual.removido, sizeof(char), 1, arquivoBinario);
+    fwrite(&registroAtual.grupo, sizeof(int), 1, arquivoBinario);
+    fwrite(&registroAtual.popularidade, sizeof(int), 1, arquivoBinario);
+    fwrite(&registroAtual.peso, sizeof(int), 1, arquivoBinario);
+    fwrite(&registroAtual.TecnologiaOrigem.tamanho, sizeof(int), 1, arquivoBinario);
+    fwrite(registroAtual.TecnologiaOrigem.string, sizeof(char), registroAtual.TecnologiaOrigem.tamanho, arquivoBinario);
+    fwrite(&registroAtual.TecnologiaDestino.tamanho, sizeof(int), 1, arquivoBinario);
+    fwrite(registroAtual.TecnologiaDestino.string, sizeof(char), registroAtual.TecnologiaDestino.tamanho, arquivoBinario);
+    int tam_bytes = sizeof(registroAtual.grupo)+sizeof(registroAtual.popularidade)+sizeof(registroAtual.peso)+sizeof(registroAtual.TecnologiaOrigem.tamanho)+sizeof(registroAtual.TecnologiaDestino.tamanho)+sizeof(char)+strlen(registroAtual.TecnologiaDestino.string)+strlen(registroAtual.TecnologiaOrigem.string);
     
     for(int i = tam_bytes; i <= TAM_REGISTRO; i++){
       char lixo = '$';
       fwrite(&lixo, sizeof(char), 1, arquivoBinario);
     }
+    cabecalho.proxRRN++;
   }
   
   free(linha);
 
-  // cabecalho.status = '1';
-  // escreveCabecalho(arquivoBinario, cabecalho);
-  // printf("nroTec: %d, nroPares: %d\n", cabecalho.nroTecnologias, cabecalho.nroParesTecnologias);
-  
+  cabecalho.status = '1';
+  escreveCabecalho(arquivoBinario, cabecalho);  
   fclose(arquivoBinario);
 
   binarioNaTela("arquivoBinario.bin");
-
-  //reescrever cabeçalho com valores atualizados (status vai para 1, contagem de tecnologias, contagem dos pares, contar proxRRN)
 }
 
 void imprimeArquivo(){
-  //incluir mensagens de erro
+  //incluir mensagens de erro - erro de nao registros ja incluido
   FILE *arquivoBinario = fopen("arquivoBinario.bin", "rb");
 
   Registro registro;
 
   //imprimir cbeçlho antes do while
-  fseek(arquivoBinario, TAM_CABECALHO, SEEK_SET);
+  fseek(arquivoBinario, 1, SEEK_SET);
+  int proxRRN;
+  fread(&proxRRN, sizeof(int), 1, arquivoBinario);
+  if(proxRRN < 1){
+    printf("Registro inexistente.\n");
+    return;
+  }
+  fseek(arquivoBinario, 8, SEEK_CUR);
   
   while(1){
     if(fread(&registro.removido, sizeof(char), 1, arquivoBinario) == 0){
@@ -309,22 +292,33 @@ void imprimeArquivo(){
     
     fread(&registro.TecnologiaOrigem.tamanho, sizeof(int), 1, arquivoBinario);
 
-    registro.TecnologiaOrigem.string = malloc((registro.TecnologiaOrigem.tamanho + 1) * sizeof(char));
-    fread(registro.TecnologiaOrigem.string, sizeof(char), registro.TecnologiaOrigem.tamanho, arquivoBinario);
-    registro.TecnologiaOrigem.string[registro.TecnologiaOrigem.tamanho] = '\0';
+    if(registro.TecnologiaOrigem.tamanho != 0){
+      registro.TecnologiaOrigem.string = malloc((registro.TecnologiaOrigem.tamanho + 1) * sizeof(char));
+      fread(registro.TecnologiaOrigem.string, sizeof(char), registro.TecnologiaOrigem.tamanho, arquivoBinario);
+      registro.TecnologiaOrigem.string[registro.TecnologiaOrigem.tamanho] = '\0';
+    }
+    else {
+      registro.TecnologiaOrigem.string = "NULO";
+    }
 
     fread(&registro.TecnologiaDestino.tamanho, sizeof(int), 1, arquivoBinario);
-    
-    registro.TecnologiaDestino.string = malloc((registro.TecnologiaDestino.tamanho + 1) * sizeof(char));
-    fread(registro.TecnologiaDestino.string, sizeof(char), registro.TecnologiaDestino.tamanho, arquivoBinario);
-    registro.TecnologiaDestino.string[registro.TecnologiaDestino.tamanho] = '\0';
 
+    if(registro.TecnologiaDestino.tamanho != 0){
+      registro.TecnologiaDestino.string = malloc((registro.TecnologiaDestino.tamanho + 1) * sizeof(char));
+      fread(registro.TecnologiaDestino.string, sizeof(char), registro.TecnologiaDestino.tamanho, arquivoBinario);
+      registro.TecnologiaDestino.string[registro.TecnologiaDestino.tamanho] = '\0';
+    }
+    else {
+      registro.TecnologiaDestino.string = "NULO";
+    }
+      
     int tam_bytes = sizeof(registro.grupo)+sizeof(registro.popularidade)+sizeof(registro.peso)+sizeof(registro.TecnologiaOrigem.tamanho)+sizeof(registro.TecnologiaDestino.tamanho)+sizeof(char)+registro.TecnologiaOrigem.tamanho+registro.TecnologiaDestino.tamanho;
     fseek(arquivoBinario, TAM_REGISTRO - tam_bytes + 1, SEEK_CUR);
 
     printf("%s, %d, %d, %s, %d\n", registro.TecnologiaOrigem.string, registro.grupo, registro.popularidade, registro.TecnologiaDestino.string, registro.peso);
-    free(registro.TecnologiaOrigem.string);
-    free(registro.TecnologiaDestino.string);
+    // printf("%c, %d, %d, %d, %d, %s, %d, %s\n", registro.removido, registro.grupo, registro.popularidade, registro. peso, registro.TecnologiaOrigem.tamanho, registro.TecnologiaOrigem.string, registro.TecnologiaDestino.tamanho, registro.TecnologiaDestino.string);
+      free(registro.TecnologiaOrigem.string);
+      free(registro.TecnologiaDestino.string);
   }
 
   fclose(arquivoBinario);
@@ -338,8 +332,8 @@ int main() {
 
   char* colunas[5];
   FILE *arquivoEntrada;
-  arquivoEntrada = fopen("tecnologia.csv", "r");
-  // arquivoEntrada = fopen("dados1.csv", "r");
+  // arquivoEntrada = fopen("tecnologia.csv", "r");
+  arquivoEntrada = fopen("dados1.csv", "r");
   // arquivoEntrada = fopen("dados2.csv", "r");
   createTable(arquivoEntrada, colunas);
   fclose(arquivoEntrada);
